db63cb377ab489c725bf2e2042b6dde2c58577bd
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-11 17:45:01 +0600

    Create code TASK002_S_1

--- TASK002_S_1/Program.cs
+++ TASK002_S_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+a = 4;
+int b;
+b = 5;
+int c;
+c = 78;
+int max;
+max = 0;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    Console.WriteLine(max);
+}
+else
+{
+    max = c;
+}
+Console.WriteLine(max);
