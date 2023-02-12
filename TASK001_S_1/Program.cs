2edbee519fd5f1dcdab402c9cda4bddf15d7547e
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-12 13:16:26 +0600

    Add task description in TASK001_S_1

--- TASK001_S_1/Program.cs
+++ TASK001_S_1/Program.cs
@@ -1,4 +1,11 @@
-﻿double a;
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+double a;
 Console.WriteLine("Введите а: ");
 a = Convert.ToDouble(Console.ReadLine());
 double b;
